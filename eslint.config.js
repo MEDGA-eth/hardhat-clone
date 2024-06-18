8fba4a9c6639342bae4684dd20245173d2b72906
William Aaron Cheung <git@troublor.xyz>
2024-06-18 12:24:12 -0500

    update test framework

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,17 @@
 // @ts-check
 
+import { includeIgnoreFile } from "@eslint/compat";
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-);
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const gitignorePath = path.resolve(__dirname, ".gitignore");
+
+export default
+  tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    includeIgnoreFile(gitignorePath),
+  );
---
+++ types/mocha/index.d.ts
@@ -0,0 +1,8 @@
+declare namespace Mocha {
+  interface MochaOptions { }
+}
+declare module "mocha" {
+  namespace Mocha {
+    interface MochaOptions { }
+  }
+}
